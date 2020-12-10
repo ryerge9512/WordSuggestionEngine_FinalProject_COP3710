34d3f758d921f68c71084df96c6d5d71ed26a98f
Ryan Yerge <ryerge9512@eagle.fgcu.edu>
2020-12-10 11:26:57 -0500

    modified Main.java to accept user file path

--- src/Main.java
+++ src/Main.java
@@ -12,23 +12,19 @@ public class Main {
    * @param args default.
    */
   public static void main(String[] args) throws IOException {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Please enter file path: ");
 
-    String filePath =
-            "/Users/ryanyerge/Documents/FGCU/WordSuggestionEngine_FinalProject_COP3710/" +
-                    "src/messages.txt";
-
-//        "C:\\Users\\Owner\\Desktop\\FGCU\\Spring_2020\\IntroToDataEngineering"
-//            + "\\WordSuggestionEngine_FinalProject_COP3710\\src\\messages.txt";
+    // ./messages.txt
+    String filePath = sc.nextLine();
 
     Tokenizer messages = new Tokenizer(filePath);
     List<String> tokens = messages.getTokens();
     Vectorizer analysis = new Vectorizer(tokens);
 
-    Scanner sc = new Scanner(System.in);
     System.out.println("Enter a word: ");
 
     String input = sc.nextLine();
-
     WordGenerator suggestedWords = new WordGenerator(analysis, input);
     suggestedWords.showWords();
   }
